f336419533ea9a68b261a3b03300cc70c3515507
bczhc <bczhc0@126.com>
2023-06-15 18:36:57 +0800

    migrate to Vite

--- src/index.js
+++
@@ -1,2 +0,0 @@
-window.$ = require('jquery')
-import "@lottiefiles/lottie-player"
---
+++ vite.config.ts
@@ -0,0 +1,20 @@
+import {defineConfig} from 'vite'
+import vue from '@vitejs/plugin-vue'
+import {resolve} from "path";
+
+// https://vitejs.dev/config
+export default defineConfig({
+    plugins: [vue()],
+    build: {
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, 'index.html'),
+                html1: resolve(__dirname, 'src/092-break-up.html'),
+                html2: resolve(__dirname, 'src/authentication.html'),
+                html3: resolve(__dirname, 'src/lottie.html'),
+                html4: resolve(__dirname, 'src/text-transfer.html'),
+                html5: resolve(__dirname, 'src/type-recorder.html'),
+            },
+        },
+    },
+})
