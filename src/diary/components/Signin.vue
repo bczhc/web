3a7b3fe3ae6abcd788f9c95fa062e6db128e5ecc
Zhai Can <bczhc0@126.com>
2023-12-18 23:31:22 +0800

    diary: use a form to log in and authenticate

--- src/diary/components/Signin.vue
+++ src/diary/components/Signin.vue
@@ -1,34 +1,15 @@
 <script setup lang="ts">
-import {ref} from "vue";
-import * as $ from 'jquery';
-
-let username = ref<string>();
-let password = ref<string>();
-
-function submitClick() {
-  $.ajax({
-    method: 'GET',
-    url: '/api/demo',
-    async: true,
-    success: data => {
-      console.log(data);
-    },
-    error: e => {
-      console.log(e);
-    }
-  });
-}
 </script>
 
 <template>
-  <div id="form">
+  <form id="form" method="post" action="/api/diary/session" enctype="application/x-www-form-urlencoded">
     <n-space vertical justify="center">
       <n-h1 style="text-align: center">Sign in</n-h1>
-      <n-input placeholder="Username" v-model:value="username"></n-input>
-      <n-input placeholder="Password" v-model:value="password"></n-input>
+      <n-input placeholder="Username" :input-props="{name: 'username'}"></n-input>
+      <n-input placeholder="Password" :input-props="{name: 'password'}" show-password-on="click" type="password"></n-input>
       <div style="text-align: center;">
-        <n-button attr-type="submit" @click="submitClick">Sign in</n-button>
+        <n-button attr-type="submit">Sign in</n-button>
       </div>
     </n-space>
-  </div>
+  </form>
 </template>
