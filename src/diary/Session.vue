cd27de66e8438715b11d5f6d6eb50f648062c8c8
Zhai Can <bczhc0@126.com>
2023-12-19 19:49:54 +0800

    diary: handle login failure - back to /signin

--- src/diary/Session.vue
+++ src/diary/Session.vue
@@ -4,7 +4,7 @@ import Centered from "./components/Centered.vue";
 
 interface Extras {
   succeeded: boolean,
-  username: string,
+  username?: string,
 }
 
 let route = useRoute();
@@ -14,13 +14,20 @@ let extras: Extras = JSON.parse(route.query['extras'] as string);
 
 window.onload = () => {
   setTimeout(() => {
-    router.push('home').then(() => router.go(0));
+    router.push(extras.succeeded ? 'home' : 'signin').then(() => {
+          if (extras.succeeded) router.go(0)
+        }
+    );
   }, 2000);
 }
 </script>
 <template>
-  <Centered>
-    <n-h1>你好，{{ extras.username }}</n-h1>
+  <Centered v-if="extras.succeeded">
+    <n-h1>你好，{{ extras.username!! }}</n-h1>
     <n-h2>马上前往主页……</n-h2>
+  </Centered>
+  <Centered v-else>
+    <n-h1>账号或密码错误。</n-h1>
+    <n-h2>正在返回登录页面……</n-h2>
   </Centered>
 </template>
